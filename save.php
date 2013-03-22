63cc92f1ec1a529b115c88d53366c1d11de7ebad
Edwin Gonzales Melquiades <edwinpgm@outlook.com>
2013-03-22 18:01:29 -0500

    sadsad

--- save.php
+++ save.php
@@ -4,10 +4,10 @@ $data = $_POST['data'];
 $file = 'qaysen.png';
 
 // remove "data:image/png;base64,"
-$uri =  substr($data,strpos($data,",")+1);
+//$uri =  substr($data,strpos($data,",")+1);
 
 // save to file
-file_put_contents($file, base64_decode($uri));
+//file_put_contents($file, base64_decode($uri));
 
 // return the filename
 echo $file;
