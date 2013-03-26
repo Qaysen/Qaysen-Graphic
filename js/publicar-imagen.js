da4f1cec2dd3c30892ee1b936a7901808d23754c
Edwin Gonzales Melquiades <edwinpgm@outlook.com>
2013-03-26 11:27:42 -0500

    sadsa

--- js/publicar-imagen.js
+++ js/publicar-imagen.js
@@ -13,15 +13,10 @@ function publicarImagen()
     {
       var uid = response.authResponse.userID;
       var accessToken = response.authResponse.accessToken;
-      FB.api('/me', function(response) 
-      {
-         console.log('Estas registrado');
-      });
 
       function descargar() 
       {
           dataURL = $("canvas").getCanvasImage("png");
-          //var dataURL = canvas.toDataURL("image/png");
           console.log(dataURL);
           //document.getElementById('canvasImg').src = dataURL;
 
@@ -32,7 +27,7 @@ function publicarImagen()
               success: function(data) {
                   console.log(data);
                   $('#publicarFB').html(data);
-                  var body = 'Sube tus imagenes y compartelas en tu muro! Ingresa a --> <a href="http://ver-novelas.com/qaysen/">Haz tu meme</a>';
+                  var body = 'Sube tus imagenes y compartelas en tu muro! Ingresa a Haz tu meme</a>';
                   var imagen = 'http://ver-novelas.com/qaysen/' + document.getElementById('publicarFB').innerHTML;
                   FB.api('/photos', 'post', {
                       message:body,
@@ -53,26 +48,7 @@ function publicarImagen()
               }
           });
       }
-
-      //Compartir imagen en el muro
       descargar();
-      //var body = 'Mi primera iamgen subida';
-      //var imagen = 'http://ver-novelas.com/qaysen/' + document.getElementById('publicarFB').innerHTML;
-      //var imagen = 'http://localhost/Qaysen-Graphic/' + document.getElementById('publicarFB').innerHTML;
-      
-      /*FB.api('/photos', 'post', {
-          message:body,
-          url:imagen        
-      }, function(response){
-
-          if (!response || response.error) {
-              alert('Error occured' + response.error);
-          } else {
-              alert('Post ID: ' + response.id);
-          }
-
-      });
-      console.log(imagen);*/
     }
     else if (response.status === 'not_authorized') 
     {
@@ -85,11 +61,3 @@ function publicarImagen()
   });
 
 };
-
-(function(d, debug){
-   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement('script'); js.id = id; js.async = true;
-   js.src = "//connect.facebook.net/es_LA/all" + (debug ? "/debug" : "") + ".js";
-   ref.parentNode.insertBefore(js, ref);
- }(document, /*debug*/ false));
