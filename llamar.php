a3481a8f596445fe1f05190e7ea2165cd5257891
Elicia <elicia@elicia-LAPTOP.(none)>
2013-03-26 17:04:34 -0500

    conslider

--- llamar.php
+++ llamar.php
@@ -5,9 +5,9 @@
 <body>
 	<p>dasdsadas</p>
 <?php 
-$dbc=mysql_connect('localhost','root','123');
-mysql_select_db('imagen');
-$query="SELECT * FROM imagenv001 ORDER BY nombre DESC";
+ include('conexion.php');
+
+$query="SELECT * FROM imagen ORDER BY nombre DESC";
 if($r=mysql_query($query,$dbc)){
 
 	while($row=mysql_fetch_array($r)){
