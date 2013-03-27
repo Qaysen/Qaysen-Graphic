f8da884ef9dcac174d22fade13d6acd163f988a0
Elicia <elicia@elicia-LAPTOP.(none)>
2013-03-26 19:15:40 -0500

    conslider

--- conexion.php
+++ conexion.php
@@ -1,15 +1,14 @@
 <?
 $dbhost="localhost"; 
 $dbuser="root"; 
-$dbpass="123456"; 
+$dbpass="123"; 
 $db="memes";
 
 $dbc=mysql_connect("$dbhost","$dbuser","$dbpass");
 mysql_select_db("$db"); 
 
-<<<<<<< HEAD
-=======
+
 session_start();
 
->>>>>>> 102f12a7a2946e4e729c6a6b8582fa8de62380dc
+
 ?>
