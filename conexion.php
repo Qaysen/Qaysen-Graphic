91601b067b06fc6d562855d473915a0ac71cb57b
root <root@ns238238.ovh.net>
2013-04-10 02:07:51 +0200

    asdasd

--- conexion.php
+++ conexion.php
@@ -1,13 +1,7 @@
 <?php
-<<<<<<< HEAD
 	$dbhost="localhost";
 	$dbuser="edwinpgm1";
-	$dbpass="melquiades"; 
-=======
-	$dbhost="localhost"; 
-	$dbuser="root"; 
-	$dbpass="123"; 
->>>>>>> bd52dec67cec61afaf549a87ce549e74001e6de6
+	$dbpass="melquiades";
 
 	$db="memes";
 
